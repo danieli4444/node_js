23faefb34f771b48ae514a262dfc2f085091893a
Daniel Yoffe <daniel.yoffe@solaredge.com>
2023-07-04 21:49:19 +0300

    express simple get

---
+++ app.js
@@ -0,0 +1,12 @@
+ const express = require('express');
+
+ const app = express();
+
+ app.listen(3000);
+
+ app.get('/',(req, res) => {
+    res.sendFile('./index.html',{root: __dirname});
+ })
+ app.get('/about',(req, res) => {
+    res.sendFile('/about.html',{root: __dirname});
+ })
